b80ba657509a2dab821ca3b65e4083bef9520fe4
Kacper Kuroczycki <kacper.kuroczycki@gmail.com>
2023-02-16 22:41:16 +0100

    main.cpp

--- main.cpp
+++ main.cpp
@@ -5,11 +5,23 @@
 using namespace std;
 FILE* y;
 FILE x;
+int z;
+long a;
+int* b;
+long* c;
 int main () {
     ifstream MyReadFile("input.txt");
     printf("wielkość typu zmiennej FILE wynosi");
     cout << sizeof(x) << endl;
     printf("wielkość typu zmiennej *FILE wynosi");
     cout << sizeof(y) << endl;
+    printf("wielkość typu zmiennej int wynosi");
+    cout << sizeof(z) << endl;
+    printf("wielkość typu zmiennej long wynosi");
+    cout << sizeof(a) << endl;
+    printf("wielkość typu zmiennej int* wynosi");
+    cout << sizeof(b) << endl;
+    printf("wielkość typu zmiennej long* wynosi");
+    cout << sizeof(c) << endl;
     return 0;
 }
